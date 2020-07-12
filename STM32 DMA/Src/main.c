e9bc12f9a7bb92ab4628e31340b44483119668b6
Phan Ngoc Cao Huy <jimmyhuy1997@gmail.com>
2020-07-12 23:19:54 +0700

    Update Matric

--- STM32 DMA/Src/main.c
+++ STM32 DMA/Src/main.c
@@ -23,12 +23,21 @@ osThreadId Task_Check_RFID_Handle;
 osThreadId calculator_Dijkstra_Handle;
 osThreadId Taskmove_Handle;
 
-static uint64_t ID_Matrix[5][5] = {
+static uint64_t ID_Matrix[5][5] = 
+{
     {0xd9c86f81ff, 0x39a5ae82b0, 0x39d39481ff, 0xb9326a8263, 0x298f6d814a},
     {0x796ebb812d, 0x192f6c82d8, 0xc95e678272, 0x199fdc82d8, 0xd7440b3ea6},
     {0x99c7dd3bb8, 0x77f488353e, 0x9725083d87, 0xb98d6e4319, 0x69236f88ad},
     {0x2952ab8151, 0x49d8948184, 0xe92d628224, 0x59dfc683c3, 0x6969818100},
     {0x9fdca82bc, 0x998cca825d, 0xa91e6d8258, 0x17c10e3ee6, 0x896f0dc52e},
+};
+uint8_t Matric[5][5] =
+{ 
+    {	1,	1,	1,	1,	1,	},
+    {	1,	1,	1,	1,	1,	},
+    {	1,	1,	1,	1,	1,	},
+    {	1,	1,	1,	1,	1,	},
+    {	1,	1,	1,	1,	1,	} 
 };
 uint8_t bufferTX[10], bufferRX[10], move = 0;
 uint64_t ID = 0;
